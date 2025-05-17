1236c97ca81c1553f93c81e4a28d6f2c44f06f4c
manuelieg <manue.espinozag@duocuc.cl>
2025-05-17 18:40:09 -0400

    Primer commit

---
+++ src/main/java/FullstackPrueba2/example/FullstackPrueba2/FullstackPrueba2Application.java
@@ -0,0 +1,13 @@
+package FullstackPrueba2.example.FullstackPrueba2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackPrueba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackPrueba2Application.class, args);
+	}
+
+}
---
+++ src/test/java/FullstackPrueba2/example/FullstackPrueba2/FullstackPrueba2ApplicationTests.java
@@ -0,0 +1,13 @@
+package FullstackPrueba2.example.FullstackPrueba2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackPrueba2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
