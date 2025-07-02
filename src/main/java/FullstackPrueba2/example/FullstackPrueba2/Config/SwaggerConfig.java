b3d7f0030f3a8bfb0e7b6eedf62ee39c32ea2918
manuelieg <manue.espinozag@duocuc.cl>
2025-07-01 22:04:56 -0400

    se agrega swagger y openapi

---
+++ src/main/java/FullstackPrueba2/example/FullstackPrueba2/Config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package FullstackPrueba2.example.FullstackPrueba2.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(
+                new Info().title("Spring API  Duoc").version("1.0.0").description("Backend Fullstack I para implementacion semestral")
+        );
+    }
+}
--- src/main/java/FullstackPrueba2/example/FullstackPrueba2/controller/UsuarioController.java
+++ src/main/java/FullstackPrueba2/example/FullstackPrueba2/controller/UsuarioController.java
@@ -2,17 +2,24 @@ package FullstackPrueba2.example.FullstackPrueba2.controller;
 
 import FullstackPrueba2.example.FullstackPrueba2.model.Usuario;
 import FullstackPrueba2.example.FullstackPrueba2.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name="Controlador Usuarios",description = "Servicio de gestion de usuarios Fullstack I")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping
+    @Operation(summary = "Obtener Usuarios",description = "Obtiene la lista de usuarios existentes en el sistema")
+    @ApiResponse(responseCode="200",description = "Consulta Exitosa")
     public String getUsuario() {
         return usuarioService.listarUsuarios();
     }
