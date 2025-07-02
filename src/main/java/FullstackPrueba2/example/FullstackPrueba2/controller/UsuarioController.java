5b974c01ce8efef5ae67ee266ef82b72bace4532
manuelieg <manue.espinozag@duocuc.cl>
2025-07-01 23:48:04 -0400

    se actualiza el controlador de usuario

--- src/main/java/FullstackPrueba2/example/FullstackPrueba2/controller/UsuarioController.java
+++ src/main/java/FullstackPrueba2/example/FullstackPrueba2/controller/UsuarioController.java
@@ -2,7 +2,9 @@ package FullstackPrueba2.example.FullstackPrueba2.controller;
 
 import FullstackPrueba2.example.FullstackPrueba2.model.Usuario;
 import FullstackPrueba2.example.FullstackPrueba2.service.UsuarioService;
-import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,17 +24,42 @@ public class UsuarioController {
 
     @GetMapping
     @Operation(summary = "Obtener Usuarios",description = "Obtiene la lista de usuarios existentes en el sistema")
-    @ApiResponse(responseCode="200",description = "Consulta Exitosa")
-    public ResponseEntity<List<Usuario>> getUsuario() {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Retorna lista completa de usuarios"),
+            @ApiResponse(responseCode = "404",description = "No se encuentran datos")
+    })
+    public ResponseEntity<List<Usuario>> getAllUsuarios() {
         List<Usuario> lista = usuarioService.listarUsuarios();
-        if (!lista.isEmpty()) {
+        if (lista.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
             return new ResponseEntity<>(lista, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary="Buscar Usuario por ID",description = "Obtiene un usuario segun el ID registrado en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Retorna Usuario"),
+            @ApiResponse(responseCode = "404",description = "No se encuentran datos")
+    })
+    @Parameter(description = "El ID del usuario", example = "123")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable int id) {
+        if (usuarioService.obtenerUsuarioPorID(id).isPresent()) {
+            return new ResponseEntity<>(usuarioService.obtenerUsuarioPorID(id).get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
+    @Operation(summary = "Agregar Usuario",description = "Permite registrar un usuario en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Usuario creado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "204",description = "No hay contenido en la solicitud")
+    })
     public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario) {
         usuarioService.agregarUsuario(usuario);
         if (usuarioService.obtenerUsuarioPorID(usuario.getId()).isPresent()) {
@@ -42,17 +69,15 @@ public class UsuarioController {
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getUsuarioById(@PathVariable int id) {
-        if (usuarioService.obtenerUsuarioPorID(id).isPresent()) {
-            Usuario us = usuarioService.obtenerUsuarioPorID(id).get();
-            return new ResponseEntity<>(us, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar Usuario",description = "Permite actualizar los datos de un usuario segun su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Usuario modificado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "204",description = "No hay contenido en la solicitud")
+    })
+    @Parameter(description = "El ID del usuario", example = "123")
     public ResponseEntity<Usuario> updateUsuario(@PathVariable int id, @RequestBody Usuario usuario) {
         if (usuarioService.obtenerUsuarioPorID(id).isPresent()) {
             usuarioService.actualizarUsuario(id, usuario);
@@ -63,7 +88,13 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Integer> deleteUsuario(@PathVariable int id) {
+    @Operation(summary="Eliminar Usuario por ID",description = "Elimina un usuario segun el ID registrado en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Retorna Usuario"),
+            @ApiResponse(responseCode = "404",description = "No se encuentran datos")
+    })
+    @Parameter(description = "El ID del usuario", example = "123")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable int id) {
         if (usuarioService.obtenerUsuarioPorID(id).isPresent()) {
             usuarioService.eliminarUsuario(id);
             return new ResponseEntity<>(HttpStatus.OK);
